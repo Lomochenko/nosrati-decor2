db7e53cbbcfccc3229819d4dff83ef93d258c72c
Lomochenko <ali.naseri3179@gmail.com>
2025-02-16 19:25:28 +0330

    fix footer position in app.vue

--- nosratiDecor/src/App.vue
+++ nosratiDecor/src/App.vue
@@ -14,9 +14,9 @@ import Footer from './components/FixFooter.vue'
           <Header />
           <Animation />
         </div>
+        <Footer />
       </div>
     </main>
-    <Footer />
 
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
